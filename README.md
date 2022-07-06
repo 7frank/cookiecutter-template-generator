c732e259c876cb3726475b5d20039df4b93b0074
7frank <>
2022-07-06 21:19:24 +0200

    add additional prerequisites

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ A meta generator for [cookiecutter](https://github.com/cookiecutter/cookiecutter
 
 - initial goal: have a bare MVP working that takes a configuration file, which will create a cookiecutter template from existing source (or project), which again can be used with cookiecutter to create a new project.
 
+# development
+
+- install dependencies globally (esrun,yarn, etc.) then run `yarn` or `npm i`
+
+- run `export DEBUG=* && esrun cookie.generator.ts `
+
 # TODO
 
-- MVP
-- create cli with esrun, cmd-ts & zod
+- (1) MVP
+- (2) create cli with cmd-ts & zod
--- cookie.generator.ts
+++ cookie.generator.ts
@@ -1,3 +1,11 @@
+import glob from "glob";
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+import debug from "debug";
+
+const log = debug("c-t-g");
+
 interface Replace {
   src: string;
   trg: string;
@@ -21,23 +29,23 @@ interface CookieGenerator {
    * a path to the root of a project or root of a github repo
    */
 
-  source: String;
+  source: string;
 
   /**
    * a path to the target where the output (the template) will be generated
    */
-  target: String;
+  target: string;
 
   configuration: ConfigurationRecord;
 }
 
 const generator: CookieGenerator = {
-  source: "../../authority-tooling",
-  target: "../",
+  source: "../authority-tooling",
+  target: "./template-out",
   configuration: {
     main: {
-      include: ["*CaseFileQueryAuthorizationService*"],
-      exclude: [],
+      include: ["**/*CaseFileQueryAuthorizationService*"],
+      exclude: [".git"],
 
       replace: [
         {
@@ -48,3 +56,25 @@ const generator: CookieGenerator = {
     },
   },
 };
+
+log(chalk.green("Let's a go!"));
+
+const root = path.resolve(generator.source);
+
+Object.entries(generator.configuration).map(([key, config]) => {
+  Object.entries(config.include ?? []).map(([key, p]) => {
+    const pathPattern = path.resolve(root, p);
+
+    log({ pathPattern });
+
+    // options is optional
+    glob(pathPattern, {}, function (e, files) {
+      // files is an array of filenames.
+      // If the `nonull` option is set, and nothing
+      // was found, then files is ["**/*.js"]
+      // er is an error object or null.
+      log({ files, e });
+      fs.copyFileSync;
+    });
+  });
+});
