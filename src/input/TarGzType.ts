04149a71879fcc8eebdd2193fa9dda2eda8799cc
7frank <>
2022-07-11 02:06:03 +0200

    enable unpacking tar gz files

--- src/input/TarGzType.ts
+++ src/input/TarGzType.ts
@@ -3,10 +3,36 @@ import { ReadStreamType } from "./ReadStreamType";
 
 import gunzip from "gunzip-maybe";
 
-export const TarGzType = extendType(ReadStreamType, {
-  async from(inputStream) {
-    return inputStream.pipe(gunzip()).pipe(process.stdout);
+import tar from "tar-stream";
 
-    // const t = await readStreamToString(inputStream);
+export const GzType = extendType(ReadStreamType, {
+  async from(inputStream) {
+    return inputStream.pipe(gunzip());
+  },
+});
+
+export const TarGzType = extendType(GzType, {
+  async from(tarStream) {
+    var extract = tar.extract();
+
+    extract.on("entry", function (header, stream, next) {
+      console.log(header);
+
+      // header is the tar header
+      // stream is the content body (might be an empty stream)
+      // call next when you are done with this entry
+
+      stream.on("end", function () {
+        next(); // ready for next entry
+      });
+
+      stream.resume(); // just auto drain the stream
+    });
+
+    extract.on("finish", function () {
+      // all entries read
+    });
+
+    return tarStream.pipe(extract);
   },
 });
